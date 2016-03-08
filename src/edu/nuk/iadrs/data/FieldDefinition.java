f4d7999042eefbd466f7a6bc5e0164aa06b925b0
Phate334 <phate334@gmail.com>
2016-03-08 21:25:43 +0800

    日期欄位拉到最後一欄
    JAVA的String.split最後一欄如果空值的話會不計，所以PT放最後一欄有可能會有沒算到的問題，把日期放最後一欄避免掉。

--- src/edu/nuk/iadrs/data/FieldDefinition.java
+++ src/edu/nuk/iadrs/data/FieldDefinition.java
@@ -3,11 +3,11 @@ package edu.nuk.iadrs.data;
 public class FieldDefinition {
 	public static int AGE = 0;
 	public static int SEX = 1;
-	public static int DATE_MONTH = 2;
-	public static int DRUG = 3;
-	public static int PT = 4;
+	public static int DRUG = 2;
+	public static int PT = 3;
+	public static int DATE_MONTH = 4;
 	
-	public static String[] FIELD_NAME = {"AGE", "SEX", "DATE_MONTH", "DRUG", "PT"};
+	public static String[] FIELD_NAME = {"AGE", "SEX", "DRUG", "PT", "DATE_MONTH"};
 	
 	public static int[][] CUBE_TYPE = {
 		{AGE}, {SEX}, {DRUG}, {PT}, 
