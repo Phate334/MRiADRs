7ec2edd81515a7ab96e36c9461d6ee7058b43b33
Phate334 <phate334@gmail.com>
2016-03-12 00:30:10 +0800

    修改計算資料總筆數的部分

--- src/edu/nuk/iadrs/data/FieldData.java
+++ src/edu/nuk/iadrs/data/FieldData.java
@@ -8,85 +8,77 @@ import org.apache.hadoop.io.IntWritable;
 
 public class FieldData {
 	private String[] rowData = null;
-	
-	public FieldData(String line) throws ParseException{
+
+	public FieldData(String line) throws ParseException {
 		rowData = line.split("#");
-		if(rowData.length != 5)
-		{
+		if (rowData.length != 5) {
 			throw new ParseException("not enough field.", rowData.length);
 		}
-		
+
 	}
-	
-	public String getRowValue(int field)
-	{
-		try{
+
+	public String getRowValue(int field) {
+		try {
 			return rowData[field];
-		}
-		catch(ArrayIndexOutOfBoundsException e){
+		} catch (ArrayIndexOutOfBoundsException e) {
 			return null;
 		}
 	}
+
 	/**
 	 * 依照需求挑出欄位回傳Text。
+	 * 
 	 * @param type，需要的欄位index，定義在FieldDefinition中。
 	 * @return Text，hadoop writable instance。
 	 */
-	public ArrayList<String> getKey(IntWritable[] type)
-	{
-		//挑出需要的欄位
+	public ArrayList<String> getKey(IntWritable[] type) {
+		// 挑出需要的欄位
 		String[] temp = new String[rowData.length];
-		for(int i=0; i<type.length; i++)
-		{
-			temp[type[i].get()] = rowData[type[i].get()];
+		for (int i = 0; i < type.length; i++) {
+			int fieldIndex = type[i].get();
+			if (fieldIndex != FieldDefinition.ALL) {
+				temp[fieldIndex] = rowData[fieldIndex];
+			}
 		}
 		temp[FieldDefinition.DATE_MONTH] = rowData[FieldDefinition.DATE_MONTH];
-		for(int i=0; i<temp.length; i++)
-		{
-			if(temp[i] == null)
-			{
+		for (int i = 0; i < temp.length; i++) {
+			if (temp[i] == null) {
 				temp[i] = "";
 			}
 		}
 		return splitDrugPt(temp);
 	}
+
 	/**
 	 * 拆開drug和PT欄位的資料，假如需要的話。
+	 * 
 	 * @return ArrayList<String>
 	 */
-	public ArrayList<String> splitDrugPt(String[] rowData)
-	{
+	public ArrayList<String> splitDrugPt(String[] rowData) {
 		ArrayList<String> outKeys = new ArrayList<String>();
-		if(rowData[FieldDefinition.DRUG].equals("") && rowData[FieldDefinition.PT].equals(""))  //兩個欄位都不需要
+		if (rowData[FieldDefinition.DRUG].equals("") && rowData[FieldDefinition.PT].equals("")) // 兩個欄位都不需要
 		{
 			outKeys.add(String.join("#", rowData));
-		}
-		else if(!rowData[FieldDefinition.DRUG].equals("") && rowData[FieldDefinition.PT].equals(""))  //只要drug
+		} else if (!rowData[FieldDefinition.DRUG].equals("") && rowData[FieldDefinition.PT].equals("")) // 只要drug
 		{
 			String[] temp = Arrays.copyOf(rowData, rowData.length);
-			for(String drug: rowData[FieldDefinition.DRUG].split("\\$"))
-			{
+			for (String drug : rowData[FieldDefinition.DRUG].split("\\$")) {
 				temp[FieldDefinition.DRUG] = drug;
 				outKeys.add(String.join("#", temp));
 			}
-		}
-		else if(rowData[FieldDefinition.DRUG].equals("") && !rowData[FieldDefinition.PT].equals(""))  //只要PT
+		} else if (rowData[FieldDefinition.DRUG].equals("") && !rowData[FieldDefinition.PT].equals("")) // 只要PT
 		{
 			String[] temp = Arrays.copyOf(rowData, rowData.length);
-			for(String pt: rowData[FieldDefinition.PT].split("\\$"))
-			{
+			for (String pt : rowData[FieldDefinition.PT].split("\\$")) {
 				temp[FieldDefinition.PT] = pt;
 				outKeys.add(String.join("#", temp));
 			}
-		}
-		else if(!rowData[FieldDefinition.DRUG].equals("") && !rowData[FieldDefinition.PT].equals(""))  //兩種欄位都要
+		} else if (!rowData[FieldDefinition.DRUG].equals("") && !rowData[FieldDefinition.PT].equals("")) // 兩種欄位都要
 		{
 			String[] temp = Arrays.copyOf(rowData, rowData.length);
-			for(String drug: rowData[FieldDefinition.DRUG].split("\\$"))
-			{
+			for (String drug : rowData[FieldDefinition.DRUG].split("\\$")) {
 				temp[FieldDefinition.DRUG] = drug;
-				for(String pt: rowData[FieldDefinition.PT].split("\\$"))
-				{
+				for (String pt : rowData[FieldDefinition.PT].split("\\$")) {
 					temp[FieldDefinition.PT] = pt;
 					outKeys.add(String.join("#", temp));
 				}
--- src/edu/nuk/iadrs/data/FieldDefinition.java
+++ src/edu/nuk/iadrs/data/FieldDefinition.java
@@ -6,13 +6,35 @@ public class FieldDefinition {
 	public static int DRUG = 2;
 	public static int PT = 3;
 	public static int DATE_MONTH = 4;
+	public static int ALL = 5;
 	
-	public static String[] FIELD_NAME = {"AGE", "SEX", "DRUG", "PT", "DATE_MONTH"};
+	public static String[] FIELD_NAME = {"AGE", "SEX", "DRUG", "PT", "DATE_MONTH", "ALL"};
 	
 	public static int[][] CUBE_TYPE = {
 		{AGE}, {SEX}, {DRUG}, {PT}, 
 		{AGE, SEX}, {AGE, DRUG}, {AGE, PT}, {SEX, DRUG}, {SEX, PT}, {DRUG, PT}, 
 		{AGE, SEX, DRUG}, {AGE, SEX, PT}, {AGE, DRUG, PT}, {SEX, DRUG, PT}, 
-		{AGE, SEX, DRUG, PT}, {}
+		{AGE, SEX, DRUG, PT}, {ALL}
 	};
+	public static int getTypeLength()
+	{
+		return CUBE_TYPE.length;
+	}
+	public static int[] getCubeType(int index)
+	{
+		if(index>=0 && index<CUBE_TYPE.length)
+		{
+			return CUBE_TYPE[index];
+		}
+		return null;
+	}
+	
+	public static String getFieldName(int index)
+	{
+		if(index>=0 && index<CUBE_TYPE.length)
+		{
+			return FIELD_NAME[index];
+		}
+		return null;
+	}
 }
