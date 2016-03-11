c33fd67e67af73b77dd8f33f6beceda1129d865f
Phate334 <phate334@gmail.com>
2016-03-12 00:31:54 +0800

    改名、開始計算每個cube
    方法二名稱改為SingleCube，每次執行一個工作，都只計算一個cube。

--- src/edu/nuk/iadrs/mr/SingleCubeMap.java
+++ src/edu/nuk/iadrs/mr/SingleCubeMap.java
@@ -12,22 +12,19 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import edu.nuk.iadrs.data.FieldData;
 
-public class OneCubeMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class SingleCubeMap extends Mapper<LongWritable, Text, Text, IntWritable> {
 	@Override
-	protected void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		Configuration conf = context.getConfiguration();
-		
+
 		IntWritable[] fields = DefaultStringifier.loadArray(conf, "cube_type", IntWritable.class);
-		try{
+		try {
 			FieldData data = new FieldData(value.toString());
-			for(String rowData:data.getKey(fields))
-			{
+			for (String rowData : data.getKey(fields)) {
 				context.write(new Text(rowData), new IntWritable(1));
 			}
+		} catch (ParseException e) {
 		}
-		catch(ParseException e){}
-		
-		
+
 	}
 }
